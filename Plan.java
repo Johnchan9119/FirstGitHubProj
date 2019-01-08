e7ed282f7690849e7064311d9f00f2305c5cd59c
Johnchan9119 <46484571+Johnchan9119@users.noreply.github.com>
2019-01-08 19:48:14 +0800

    Add files via upload

---
+++ Plan.java
@@ -0,0 +1,7 @@
+public class Plan{
+	public static void main(String[]args){
+		System.out.println("1、每天听课2小时，边学边练习，练习至少4小时");
+		System.out.println("2、学习时间：早上：8:00-11:00,晚上:7:00-10:00");
+		System.out.println("3、本周学完JAVA基础语法课程的专题1和专题2，阅读完教材，完成所有练习加油！");
+	}
+}
